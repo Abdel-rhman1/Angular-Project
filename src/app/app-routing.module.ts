eacdd1f8eae0bb046600d2e8f7619930bfdaf7de
Abdel-rhman1 <yousef777906@gmail.com>
2021-09-09 13:51:59 +0200

    adding Project For First Time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { MovicesComponent } from './movices/movices.component';
+import { NetworkComponent } from './network/network.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { TvComponent } from './tv/tv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : '' , redirectTo:'home' , pathMatch:'full'},
+{path:'home' , component:HomeComponent},
+{path : 'about' , component:AboutComponent},
+{path : 'tv' , component:TvComponent},
+{path : 'network' , component:NetworkComponent},
+{path: 'movice' , component:MovicesComponent},
+{path: '**' , component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TvComponent } from './tv/tv.component';
+import { MovicesComponent } from './movices/movices.component';
+import { NetworkComponent } from './network/network.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TvComponent,
+    MovicesComponent,
+    NetworkComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +34,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+{ }
